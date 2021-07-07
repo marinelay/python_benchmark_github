b42865140b44f35782404322f74b61e3c3a0e10f
Wonseok <marinelay7@gmail.com>
2021-07-07 16:18:28 +0900

    modify for using only script file

--- benchmark.sh
+++ benchmark.sh
@@ -1,27 +1,34 @@
 #! /bin/bash
+pj=$1
 
 
+jq -r '.'${pj}' | .[] | @base64' benchmark.json | while read i; do
+    _jq() {
+        echo ${i} | base64 --decode | jq -r ${1}
+    }
 
-pj_name=$1
-py_version=$2
+    pj_name=$(_jq '.name')
+    py_version=$(_jq '.python')
 
-git clone -b ${pj_name} --single-branch https://github.com/marinelay/python_benchmark_github.git
+    git clone -b ${pj_name} --single-branch https://github.com/marinelay/python_benchmark_github.git
 
-pwd=$PWD
+    pwd=$PWD
 
-if cd python_benchmark_github; then
-    pyenv install ${py_version}
-    pyenv virtualenv ${py_version} ${pj_name}
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
-    source ~/.bashrc
+    if cd python_benchmark_github; then
+        pyenv install ${py_version}
+        pyenv virtualenv ${py_version} ${pj_name}
+        eval "$(pyenv init -)"
+        eval "$(pyenv virtualenv-init -)"
+        source ~/.bashrc
 
-    pyenv activate ${pj_name}
-    pyenv local ${pj_name}
-    pip install -r requirements.txt
-    pyenv deactivate
+        pyenv activate ${pj_name}
+        pyenv local ${pj_name}
+        pip install -r requirements.txt
+        pyenv deactivate
 
-    cd ${pwd}
+        cd ${pwd}
 
-    mv python_benchmark_github ${pj_name}
-fi
+        mv python_benchmark_github ${pj_name}
+    fi
+done
+
