d6a906b5def76fe575372a2993c630d644f2c507
Wonseok <marinelay7@gmail.com>
2021-07-07 14:49:10 +0900

    bug fix on script

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+
+
 pj_name=$1
 py_version=$2
 
@@ -7,13 +9,19 @@ git clone -b ${pj_name} --single-branch https://github.com/marinelay/python_benc
 
 pwd=$PWD
 
-cd python_benchmark_github
+if cd python_benchmark_github; then
+    pyenv install ${py_version}
+    pyenv virtualenv ${py_version} ${pj_name}
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+    source ~/.bashrc
 
-pyenv install ${py_version}
-pyenv virutalenv ${py_version} ${pj_name}
-pyenv local ${pj_name}
-pip install -r requirements.txt
+    pyenv activate ${pj_name}
+    pyenv local ${pj_name}
+    pip install -r requirements.txt
+    pyenv deactivate
 
-cd ${pwd}
+    cd ${pwd}
 
-mv python_benchmark_github ${pj_name}
+    mv python_benchmark_github ${pj_name}
+fi
