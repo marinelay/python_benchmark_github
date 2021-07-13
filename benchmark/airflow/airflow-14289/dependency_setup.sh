ad605b467869a472ba6c78a91f653250596fe256
Wonseok <marinelay7@gmail.com>
2021-07-13 18:56:22 +0900

    add to 14686

--- benchmark/airflow/airflow-14289/dependency_setup.sh
+++ benchmark/airflow/airflow-14289/dependency_setup.sh
@@ -1,9 +1,10 @@
-sudo apt-get install -y apt-utils mysql-server libmysqlclient-dev libxml2 libpq-dev libkrb5-dev libsasl2-dev
+sudo apt-get install -y apt-utils mysql-server libmysqlclient-dev libxml2 libpq-dev libkrb5-dev libsasl2-dev unixodbc-dev libldap2-dev
 export SLUGIFY_USES_TEXT_UNIDECODE=yes
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
+pip install --upgrade pip
 pip install -r ../requirements.txt
 pip install -e ".[devel]"
 export PYTHONIOENCODING=utf-8
